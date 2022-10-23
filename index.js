d278c99694584dfbf94631dbed772ea939cc3687
yeungparkhay <yeungparkhay@gmail.com>
2022-10-23 10:09:35 +0100

    Updated envoy API to use Cheerio

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
+const axios = require("axios")
+const cheerio = require("cheerio")
+const fs = require("fs")
 const express = require("express")
 const bodyParser = require("body-parser")
 const cors = require("cors")
 const helmet = require("helmet")
 const morgan = require("morgan")
-const fetch = require("cross-fetch")
-const puppeteer = require("puppeteer-core")
-// const chromium = require("chrome-aws-lambda")
 const { PrismaClient } = require("@prisma/client")
+const { proposal } = require("@prisma/client")
 const Telegram = require("telegram-notify")
 require("dotenv").config()
 
@@ -26,75 +27,72 @@ app.get("/", (req, res) => {
 })
 
 app.get("/api/proposals/fetch/makerdao", async (req, res) => {
-    const scrape = async (slug) => {
-        console.log(`/api/proposals/fetch/makerdao:  Scraping ${slug} data`)
-        console.log(`/api/proposals/fetch/makerdao:  Launching browser`)
-        const browser = await puppeteer.launch({
-            args: ["--hide-scrollbars", "--disable-web-security", "--no-sandbox", "--disable-setuid-sandbox"],
-            // defaultViewport: chromium.defaultViewport,
-            // executablePath: await chromium.executablePath,
-            headless: true,
-            ignoreHTTPSErrors: true,
+    const pollUrl = "https://vote.makerdao.com/polling"
+    const executiveUrl = "https://vote.makerdao.com/executive"
+
+    const fetchJSON = async (url) => {
+        console.log(`/api/proposals/fetch/makerdao:  Scraping poll data from ${url}`)
+        const { data: html } = await axios.get(url)
+        const $ = cheerio.load(html)
+        const results = $("#__NEXT_DATA__").text()
+        fs.writeFileSync("results.json", results)
+        let json = JSON.parse(fs.readFileSync("results.json", "utf8"))
+        return json
+    }
+
+    const processPollsJSON = async (json) => {
+        const entries = json.props.pageProps.polls
+        const data = entries.map((p) => {
+            return {
+                title: p.title,
+                type: "Poll",
+                voteType: p.parameters.inputFormat.type
+                    .split("-")
+                    .map((word) => word[0].toUpperCase() + String(word).substring(1))
+                    .join(" "),
+                options: Object.values(p.options),
+                dateAdded: p.startDate,
+                dateExpiry: p.endDate,
+                voteUrl: `https://vote.makerdao.com/polling/${p.slug}`,
+                forumUrl: p.discussionLink,
+                status: "Unassigned",
+            }
         })
-
-        const page = await browser.newPage()
-
-        await page.goto(`https://vote.makerdao.com/${slug}`)
-        const element = await page.waitForSelector("#__NEXT_DATA__")
-        const text = await page.evaluate((element) => element?.textContent, element)
-
-        browser.close()
-        console.log(`/api/proposals/fetch/makerdao:  Closed browser`)
-
-        if (!text) return []
-
-        const entries = JSON.parse(text).props.pageProps[slug === "polling" ? "polls" : "proposals"]
-        const data =
-            slug === "polling"
-                ? entries.map((p) => {
-                      return {
-                          title: p.title,
-                          type: "Poll",
-                          voteType: p.parameters.inputFormat.type
-                              .split("-")
-                              .map((word) => word[0].toUpperCase() + String(word).substring(1))
-                              .join(" "),
-                          options: Object.values(p.options),
-                          dateAdded: p.startDate,
-                          dateExpiry: p.endDate,
-                          voteUrl: `https://vote.makerdao.com/${slug}/${p.slug}`,
-                          forumUrl: p.discussionLink,
-                          status: "Unassigned",
-                      }
-                  })
-                : entries.map((p) => {
-                      return {
-                          title: p.title,
-                          type: "Executive Proposal",
-                          voteType: "Executive Proposal",
-                          options: [""],
-                          dateAdded: p.spellData.datePassed,
-                          dateExpiry: p.spellData.expiration,
-                          dateExecuted: p.spellData.dateExecuted,
-                          voteUrl: `https://vote.makerdao.com/${slug}/${"template-executive-vote-"}${String(p.title)
-                              .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
-                              .replace(/ +(?= )/g, "")
-                              .toLowerCase()
-                              .replace(/ /g, "-")}${"#proposal-detail"}`,
-                          forumUrl: "",
-                          status: "Unassigned",
-                      }
-                  })
         return data
     }
 
-    const polls = await scrape("polling")
+    const processExecutiveJSON = async (json) => {
+        const entries = json.props.pageProps.proposals
+        const data = entries.map((p) => {
+            return {
+                title: p.title,
+                type: "Executive Proposal",
+                voteType: "Executive Proposal",
+                options: [""],
+                dateAdded: p.spellData.datePassed,
+                dateExpiry: p.spellData.expiration,
+                dateExecuted: p.spellData.dateExecuted,
+                voteUrl: `https://vote.makerdao.com/executive/${"template-executive-vote-"}${String(p.title)
+                    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+                    .replace(/ +(?= )/g, "")
+                    .toLowerCase()
+                    .replace(/ /g, "-")}${"#proposal-detail"}`,
+                forumUrl: "",
+                status: "Unassigned",
+            }
+        })
+        return data
+    }
+
+    const pollsJSON = await fetchJSON(pollUrl)
+    const polls = await processPollsJSON(pollsJSON)
     console.log(`/api/proposals/fetch/makerdao:  Retrieved polls`)
-    const proposals = await scrape("executive")
+    const executiveJSON = await fetchJSON(executiveUrl)
+    const executive = await processExecutiveJSON(executiveJSON)
     console.log(`/api/proposals/fetch/makerdao:  Retrieved executive proposals`)
-    const data = polls.concat(proposals)
+    const proposals = polls.concat(executive)
     console.log(`/api/proposals/fetch/makerdao:  Returning voting data`)
-    res.status(200).json(data)
+    res.status(200).json(proposals)
 })
 
 app.post("/api/proposals/save", async (req, res) => {
@@ -146,53 +144,42 @@ app.post("/api/proposals/save", async (req, res) => {
 })
 
 app.get("/api/proposals/fetch-all", async (req, res) => {
-    const proposalResponse = await fetch(`${server}/api/proposals/fetch/makerdao`, {
-        method: "GET",
+    const proposalsRequest = await axios.get(`${server}/api/proposals/fetch/makerdao`, {
         headers: {
             "Content-Type": "application/json",
             "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization",
         },
     })
-
-    const fetchedProposals = await proposalResponse.json()
+    const fetchedProposals = proposalsRequest.data
+    const newProposals = []
 
     console.log(`/api/proposals/fetch-all:  Fetched makerdao proposals`)
 
-    for (const selectedProposal of await fetchedProposals) {
-        const data = {
-            title: selectedProposal.title,
-            protocol: {
-                connect: {
-                    name: "MakerDAO",
-                },
+    for (const selectedProposal of fetchedProposals) {
+        const existingProposal = await prisma.proposal.findUnique({
+            where: {
+                title: selectedProposal.title,
             },
-            type: selectedProposal.type,
-            dateAdded: selectedProposal.dateAdded,
-            dateExpiry: selectedProposal.dateExpiry,
-            voteType: selectedProposal.voteType,
-            options: selectedProposal.options,
-            voteUrl: selectedProposal.voteUrl,
-            forumUrl: selectedProposal.forumUrl,
-            status: selectedProposal.status,
-        }
-
-        const response = await fetch(`${server}/api/proposals/save`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json, text/plain, */*",
-                "User-Agent": "*",
-                "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-            },
-            body: JSON.stringify(data),
         })
-
-        console.log(`/api/proposals/fetch-all:  Saved new proposal`)
-
-        const newProposal = await response.json()
-        if (response.status === 200) {
-            const message = `${data.title}\n\nType: ${data.type}\nVote Type: ${data.voteType}\nOptions: ${data.options}\nDate Added: ${data.dateAdded}\nExpiry date: ${data.dateExpiry}\nVote URL: ${data.voteUrl}\nForum URL: ${data.forumUrl}`
-            await notify.send(message)
+        if (!existingProposal) {
+            console.log(`/api/proposals/fetch-all:  Saving new proposal: ${selectedProposal.name}`)
+            try {
+                const response = await axios.post(`${server}/api/proposals/save`, selectedProposal, {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Accept: "application/json, text/plain, */*",
+                        "User-Agent": "*",
+                        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+                    },
+                })
+                if (response.status === 200) {
+                    const message = `${selectedProposal.title}\n\nType: ${selectedProposal.type}\nVote Type: ${selectedProposal.voteType}\nOptions: ${selectedProposal.options}\nDate Added: ${selectedProposal.dateAdded}\nExpiry date: ${selectedProposal.dateExpiry}\nVote URL: ${selectedProposal.voteUrl}\nForum URL: ${selectedProposal.forumUrl}`
+                    await notify.send(message)
+                }
+                console.log(`/api/proposals/fetch-all:  Saved new proposal`)
+            } catch (err) {
+                console.error(err)
+            }
         }
     }
     res.status(200).json({ message: "done" })

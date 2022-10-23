3b7ab523e522b240a2fe39da895b85d4c9c31e38
yeungparkhay <yeungparkhay@gmail.com>
2022-10-23 15:28:45 +0100

    Added Aave support

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ require("dotenv").config()
 
 const server = process.env.NODE_ENV === "development" ? "http://localhost:3001" : "https://envoy-apis.herokuapp.com"
 
-const notify = new Telegram({ token: process.env.BOT_TOKEN, chatId: process.env.CHAT_ID })
+const notifyMaker = new Telegram({ token: process.env.BOT_TOKEN, chatId: process.env.MAKER_CHAT_ID })
+const notifyAave = new Telegram({ token: process.env.BOT_TOKEN, chatId: process.env.AAVE_CHAT_ID })
 const prisma = new PrismaClient()
 const app = express()
 
@@ -45,6 +46,7 @@ app.get("/api/proposals/fetch/makerdao", async (req, res) => {
         const data = entries.map((p) => {
             return {
                 title: p.title,
+                protocol: "MakerDAO",
                 type: "Poll",
                 voteType: p.parameters.inputFormat.type
                     .split("-")
@@ -66,6 +68,7 @@ app.get("/api/proposals/fetch/makerdao", async (req, res) => {
         const data = entries.map((p) => {
             return {
                 title: p.title,
+                protocol: "MakerDAO",
                 type: "Executive Proposal",
                 voteType: "Executive Proposal",
                 options: [""],
@@ -95,6 +98,65 @@ app.get("/api/proposals/fetch/makerdao", async (req, res) => {
     res.status(200).json(proposals)
 })
 
+app.get("/api/proposals/fetch/aave", async (req, res) => {
+    const url = "https://app.aave.com/governance/"
+    let proposals = []
+
+    console.log(`/api/proposals/fetch/aave:  Scraping poll data from ${url}`)
+    const { data: html } = await axios.get(url)
+    const $ = cheerio.load(html)
+    $("div:not([id]):not([class])")
+        .children("a")
+        .each(async (idx, val) => {
+            const status = $(val).find("div:nth-child(1) > div > span:nth-child(1)").text()
+            const href = $(val).attr("href")
+
+            const proposalId = href.match(/\d+/g)
+            const { data: details } = await axios.get(`${server}/api/proposals/fetch/aave/${proposalId}`)
+
+            const proposal = {
+                title: $(val).find("div > h3").text(),
+                protocol: "Aave",
+                dateAdded: details.dateAdded,
+                dateExpiry: details.dateExpiry,
+                status: status ? status : "Upcoming",
+                type: "Executive Proposal",
+                voteType: "Executive Proposal",
+                options: ["Yae", "Nay"],
+                voteUrl: `https://app.aave.com${href}`,
+                forumUrl: details.forumUrl,
+                status: "Unassigned",
+            }
+            proposals.push(proposal)
+        })
+    setTimeout(() => {
+        console.log(proposals)
+        res.status(200).json(
+            proposals.sort((a, b) => {
+                return new Date(b.dateAdded) - new Date(a.dateAdded)
+            })
+        )
+    }, [5000])
+})
+
+app.get("/api/proposals/fetch/aave/:id", async (req, res) => {
+    const { id } = req.params
+    const url = `https://app.aave.com/governance/proposal/${id}`
+    let details = { dateAdded: "", dateExpiry: "", forumUrl: "" }
+
+    const { data: html } = await axios.get(url)
+    const $ = cheerio.load(html)
+    $("main > div:nth-child(2) > div > div > div:nth-child(2) > div:nth-child(3) > div").each((idx, val) => {
+        let date = $(val).find("div:nth-child(2) > p:nth-child(1)").text()
+        if (idx === 0) details.dateAdded = new Date(date)
+        if (idx === 2) details.dateExpiry = new Date(date)
+    })
+    $("main > div:nth-child(2) > div > div > div:nth-child(2) > div:nth-child(3) > div > a").each((idx, val) => {
+        if (idx === 0) details.forumUrl = $(val).attr("href")
+    })
+    res.status(200).json(details)
+})
+
 app.post("/api/proposals/save", async (req, res) => {
     const data = req.body
 
@@ -104,7 +166,7 @@ app.post("/api/proposals/save", async (req, res) => {
                 title: data.title,
                 protocol: {
                     connect: {
-                        name: "MakerDAO",
+                        name: data.protocol,
                     },
                 },
                 type: data.type,
@@ -144,24 +206,32 @@ app.post("/api/proposals/save", async (req, res) => {
 })
 
 app.get("/api/proposals/fetch-all", async (req, res) => {
-    const proposalsRequest = await axios.get(`${server}/api/proposals/fetch/makerdao`, {
+    const MakerProposalsRequest = await axios.get(`${server}/api/proposals/fetch/makerdao`, {
         headers: {
             "Content-Type": "application/json",
             "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization",
         },
     })
-    const fetchedProposals = proposalsRequest.data
+    const MakerFetchedProposals = MakerProposalsRequest.data
+    console.log(`/api/proposals/fetch-all:  Fetched MakerDAO proposals`)
 
-    console.log(`/api/proposals/fetch-all:  Fetched makerdao proposals`)
+    const AaveProposalsRequest = await axios.get(`${server}/api/proposals/fetch/aave`, {
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+        },
+    })
+    const AaveFetchedProposals = AaveProposalsRequest.data
+    console.log(`/api/proposals/fetch-all:  Fetched Aave proposals`)
 
-    for (const selectedProposal of fetchedProposals) {
+    for (const selectedProposal of MakerFetchedProposals) {
         const existingProposal = await prisma.proposal.findUnique({
             where: {
                 title: selectedProposal.title,
             },
         })
         if (!existingProposal) {
-            console.log(`/api/proposals/fetch-all:  Saving new proposal: ${selectedProposal.name}`)
+            console.log(`/api/proposals/fetch-all:  Saving new proposal`)
             try {
                 const response = await axios.post(`${server}/api/proposals/save`, selectedProposal, {
                     headers: {
@@ -174,10 +244,10 @@ app.get("/api/proposals/fetch-all", async (req, res) => {
                 if (response.status === 200) {
                     if (selectedProposal.type == "Poll") {
                         const message = `${selectedProposal.title}\n\nType: ${selectedProposal.type}\nVote Type: ${selectedProposal.voteType}\nOptions: ${selectedProposal.options}\nDate Added: ${selectedProposal.dateAdded}\nExpiry date: ${selectedProposal.dateExpiry}\nVote URL: ${selectedProposal.voteUrl}\nForum URL: ${selectedProposal.forumUrl}`
-                        await notify.send(message)
+                        await notifyMaker.send(message)
                     } else {
                         const message = `${selectedProposal.title}\n\nType: ${selectedProposal.type}\nDate Added: ${selectedProposal.dateAdded}\nExpiry date: ${selectedProposal.dateExpiry}\nVote URL: ${selectedProposal.voteUrl}`
-                        await notify.send(message)
+                        await notifyMaker.send(message)
                     }
                 }
                 console.log(`/api/proposals/fetch-all:  Saved new proposal`)
@@ -187,16 +257,49 @@ app.get("/api/proposals/fetch-all", async (req, res) => {
         }
     }
 
-    try {
-        await axios.get(`${server}/api/proposals/expiring`, {
-            headers: {
-                "Content-Type": "application/json",
-                "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+    for (const selectedProposal of AaveFetchedProposals) {
+        const existingProposal = await prisma.proposal.findUnique({
+            where: {
+                title: selectedProposal.title,
             },
         })
-    } catch (err) {
-        console.error(err)
+        if (!existingProposal) {
+            console.log(`/api/proposals/fetch-all:  Saving new proposal`)
+            try {
+                const response = await axios.post(`${server}/api/proposals/save`, selectedProposal, {
+                    headers: {
+                        "Content-Type": "application/json",
+                        Accept: "application/json, text/plain, */*",
+                        "User-Agent": "*",
+                        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+                    },
+                })
+                if (response.status === 200) {
+                    if (selectedProposal.type == "Poll") {
+                        const message = `${selectedProposal.title}\n\nType: ${selectedProposal.type}\nVote Type: ${selectedProposal.voteType}\nOptions: ${selectedProposal.options}\nDate Added: ${selectedProposal.dateAdded}\nExpiry date: ${selectedProposal.dateExpiry}\nVote URL: ${selectedProposal.voteUrl}\nForum URL: ${selectedProposal.forumUrl}`
+                        await notifyAave.send(message)
+                    } else {
+                        const message = `${selectedProposal.title}\n\nType: ${selectedProposal.type}\nDate Added: ${selectedProposal.dateAdded}\nExpiry date: ${selectedProposal.dateExpiry}\nVote URL: ${selectedProposal.voteUrl}\nForum URL: ${selectedProposal.forumUrl}`
+                        await notifyAave.send(message)
+                    }
+                }
+                console.log(`/api/proposals/fetch-all:  Saved new proposal`)
+            } catch (err) {
+                console.error(err)
+            }
+        }
     }
+
+    // try {
+    //     await axios.get(`${server}/api/proposals/expiring`, {
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //             "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+    //         },
+    //     })
+    // } catch (err) {
+    //     console.error(err)
+    // }
 
     res.status(200).json({ message: "done" })
 })
